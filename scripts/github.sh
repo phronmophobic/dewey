216151b51b75f2e3cc5107b31059b1e7063afc81
Adrian Smith <smith.adriane@gmail.com>
2023-01-14 12:54:43 -0800

    don't bootstrap inside github script.

--- scripts/github.sh
+++ scripts/github.sh
@@ -2,7 +2,4 @@
 set -e
 set -x
 
-curl -O https://raw.githubusercontent.com/phronmophobic/dewey/main/scripts/bootstrap.sh
-chmod 755 ./bootstrap.sh
-source ./bootstrap.sh
 clojure -Sdeps '{:deps {com.phronemophobic/dewey {:git/sha 3d4bfc2ade65b3b266805fd78c7d6dde9610c5d9, :git/url https://github.com/phronmophobic/dewey}}}' -M -e '(require (quote com.phronemophobic.dewey.etl))(ns com.phronemophobic.dewey.etl)(run 77540d51-0aac-4700-8e3c-458b1149d38d" \)
