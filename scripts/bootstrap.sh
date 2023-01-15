6eeeb444b06111a28c1ca98cb8147ec209563b87
Adrian Smith <smith.adriane@gmail.com>
2023-01-14 20:37:31 -0800

    specify noninteractive for apt install.

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -3,19 +3,13 @@ set -e
 set -x
 
 apt -y update
-apt -y install emacs-lucid
-apt -y install openjdk-18-jre-headless
-apt -y install rlwrap
-apt -y install awscli
-apt -y install unzip
-apt -y install leiningen
-apt -y install git
+DEBIAN_FRONTEND=noninteractive apt -y install emacs-lucid openjdk-18-jre-headless rlwrap awscli unzip leiningen git build-essential
 NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 echo '# Set PATH, MANPATH, etc., for Homebrew.' >> /home/ubuntu/.profile
 echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> /home/ubuntu/.profile
 eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 
-apt -y install build-essential
+
 brew install gcc
 brew install clojure/tools/clojure
