be904335564447ac79e09b76247bcf0ad0979a6a
Adrian Smith <smith.adriane@gmail.com>
2024-07-28 10:13:56 -0700

    update bootstrap script.

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -4,12 +4,15 @@ set -x
 
 apt -y update
 DEBIAN_FRONTEND=noninteractive apt -y install emacs-lucid openjdk-18-jre-headless rlwrap awscli unzip leiningen git build-essential
-NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-echo '# Set PATH, MANPATH, etc., for Homebrew.' >> /home/ubuntu/.profile
-echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> /home/ubuntu/.profile
-eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+wget https://download.clojure.org/install/linux-install.sh
+chmod 755 linux-install.sh
+./linux-install.sh
+clojure -P
+
+wget https://raw.githubusercontent.com/phronmophobic/dev-env/master/.simpleemacs
+mv .simpleemacs ~/.emacs
 
 
-brew install gcc
-brew install clojure/tools/clojure
+
+
