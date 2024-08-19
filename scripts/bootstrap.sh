f2a0c186a5d9d579b00455165c440492c4de4d96
Adrian Smith <smith.adriane@gmail.com>
2024-08-19 08:18:08 -0700

    use curl instead of `wget` when bootstrapping.
    wget is not always available by default.

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -5,14 +5,11 @@ set -x
 apt -y update
 DEBIAN_FRONTEND=noninteractive apt -y install emacs-lucid openjdk-18-jre-headless rlwrap awscli unzip leiningen git build-essential
 
-wget https://download.clojure.org/install/linux-install.sh
+curl -O https://download.clojure.org/install/linux-install.sh
 chmod 755 linux-install.sh
 ./linux-install.sh
 clojure -P
 
-wget https://raw.githubusercontent.com/phronmophobic/dev-env/master/.simpleemacs
+curl -O https://raw.githubusercontent.com/phronmophobic/dev-env/master/.simpleemacs
 mv .simpleemacs ~/.emacs
-
-
-
 
