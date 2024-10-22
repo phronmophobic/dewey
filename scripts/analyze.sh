1e8fe692de87b0801cb33a5fdffc1f6d9c86e496
Adrian Smith <smith.adriane@gmail.com>
2024-10-21 20:40:14 -0700

    print some debug info.

--- scripts/analyze.sh
+++ scripts/analyze.sh
@@ -7,6 +7,8 @@ RELEASE="$2"
 
 sudo apt -y install sqlite3
 
+java -XX:+PrintFlagsFinal -version 
+
 clojure -Sdeps "{:deps {com.phronemophobic/dewey {:git/sha \"${SHA}\", :git/url \"https://github.com/phronmophobic/dewey\"}}}" -M --report stderr -e "(require (quote com.phronemophobic.dewey.etl))(ns com.phronemophobic.dewey.etl)(run-index \"${RELEASE}\" )"
 
 
