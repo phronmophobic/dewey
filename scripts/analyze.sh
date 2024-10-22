b3a574ea39ce22f9f33384b9d8dcaf8e8b3a2cac
Adrian Smith <smith.adriane@gmail.com>
2024-10-22 10:11:55 -0700

    use more memory when analyzing.

--- scripts/analyze.sh
+++ scripts/analyze.sh
@@ -5,10 +5,10 @@ set -x
 SHA="$1"
 RELEASE="$2"
 
-sudo apt -y install sqlite3
+apt -y install sqlite3
 
 java -XX:+PrintFlagsFinal -version 
 
-clojure -Sdeps "{:deps {com.phronemophobic/dewey {:git/sha \"${SHA}\", :git/url \"https://github.com/phronmophobic/dewey\"}}}" -M --report stderr -e "(require (quote com.phronemophobic.dewey.etl))(ns com.phronemophobic.dewey.etl)(run-index \"${RELEASE}\" )"
+clojure -J-Xmx5g -Sdeps "{:deps {com.phronemophobic/dewey {:git/sha \"${SHA}\", :git/url \"https://github.com/phronmophobic/dewey\"}}}" -M --report stderr -e "(require (quote com.phronemophobic.dewey.etl))(ns com.phronemophobic.dewey.etl)(run-index \"${RELEASE}\" )"
 
 
