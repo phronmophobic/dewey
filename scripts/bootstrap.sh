bc86da7fa7c26fde9f6a58efebe216befafe66ca
Adrian Smith <smith.adriane@gmail.com>
2024-12-04 10:21:01 -0800

    use aws installer for aws cli.

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -5,6 +5,10 @@ set -x
 apt -y update
 DEBIAN_FRONTEND=noninteractive apt -y install emacs-lucid openjdk-21-jre-headless rlwrap unzip leiningen git build-essential
 
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+./aws/install
+
 curl -O https://download.clojure.org/install/linux-install.sh
 chmod 755 linux-install.sh
 ./linux-install.sh
