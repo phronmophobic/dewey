7f2c4c96f1d5d2f0759f84154583daaed8c19540
Adrian Smith <smith.adriane@gmail.com>
2025-06-11 10:28:04 -0700

    update github command to support datalevin.

--- scripts/github.sh
+++ scripts/github.sh
@@ -5,7 +5,7 @@ set -x
 SHA="$1"
 RELEASE="$2"
 
-clojure -Sdeps "{:deps {com.phronemophobic/dewey {:git/sha \"${SHA}\", :git/url \"https://github.com/phronmophobic/dewey\"}}}" -M --report stderr -e "(require (quote com.phronemophobic.dewey.etl))(ns com.phronemophobic.dewey.etl)(run \"${RELEASE}\" )"
+clojure -J"--add-opens=java.base/java.nio=ALL-UNNAMED" -J"--add-opens=java.base/sun.nio.ch=ALL-UNNAMED" -Sdeps "{:deps {com.phronemophobic/dewey {:git/sha \"${SHA}\", :git/url \"https://github.com/phronmophobic/dewey\"}}}" -M --report stderr -e "(require (quote com.phronemophobic.dewey.etl))(ns com.phronemophobic.dewey.etl)(run \"${RELEASE}\" )"
 
 
 
